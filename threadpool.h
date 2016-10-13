3046982f88aa0b206b68765c2a4789f3f468854c
SarahYuHanCheng <bigbigtreeful@gmail.com>
2016-10-13 14:27:52 +0800

    Add doxygen-style comment to threadpool.h

--- threadpool.h
+++ threadpool.h
@@ -5,19 +5,30 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+/**
+ * @struct _task threadpool.h "threadpool.h"
+ * @brief The data structure for a task which will be put in a task queue
+ */
 typedef struct _task {
-    void (*func)(void *);
-    void *arg;
-    struct _task *next, *last;
+    void (*func)(void *);	///<pointer to task function
+    void *arg;			///<pointer to the arguments passed to _func_
+    struct _task *next;		///<pointer to the next task
+    struct _task *last;		///<poniter to the previous task
 } task_t;
 
 int task_free(task_t *the_task);
 
-typedef struct {
-    task_t *head, *tail;
-    pthread_mutex_t mutex;
-    pthread_cond_t cond;
-    uint32_t size;
+/*======Task Queue======*/
+/**
+ * @struct _tqueue_t threadpool.h "threadpool.h"
+ * @brief The data structure for a task queue of _task\_t_
+ */
+typedef struct _tqueue_t {
+    task_t *head;		///<pointer to the first _task\_t_in in queue
+    task_t *tail;		///<pointer to the last _task\_t_in queue
+    pthread_mutex_t mutex;	///<The mutex lock of this queue
+    pthread_cond_t cond;	///<The conditional variable of this queue
+    uint32_t size;		///<The size of queue
 } tqueue_t;
 
 int tqueue_init(tqueue_t *the_queue);
@@ -26,10 +37,17 @@ uint32_t tqueue_size(tqueue_t *the_queue);
 int tqueue_push(tqueue_t *the_queue, task_t *task);
 int tqueue_free(tqueue_t *the_queue);
 
-typedef struct {
-    pthread_t *threads;
-    uint32_t count;
-    tqueue_t *queue;
+/*======Thread pool ======*/
+
+/**
+ * @struct _tpool_t threadpool.h "threadpool.h"
+ * @brief The data structure for managing thread pool
+ */
+
+typedef struct tpool_t {
+    pthread_t *threads;		///<pointer to the array of threads
+    uint32_t count;		///<Number of working threads
+    tqueue_t *queue;		///<pointer to the task queue
 } tpool_t;
 
 int tpool_init(tpool_t *the_pool, uint32_t count, void *(*func)(void *));
