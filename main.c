025693139530e1e3dc1db47fcb82151d2d794db8
SarahYuHanCheng <bigbigtreeful@gmail.com>
2016-10-14 05:10:46 +0800

    Extract the func of merge sorting from main file

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 
 #include "threadpool.h"
 #include "list.h"
-
+#include "merge_sort.h"
 #define USAGE "usage: ./sort [thread_count] [input_count]\n"
 
 struct {
@@ -17,54 +17,7 @@ static llist_t *the_list = NULL;
 
 static int thread_count = 0, data_count = 0, max_cut = 0;
 static tpool_t *pool = NULL;
-
-llist_t *merge_list(llist_t *a, llist_t *b)
-{
-    llist_t *_list = list_new();
-    node_t *current = NULL;
-    while (a->size && b->size) {
-        //Choose the linked list whose data of first node is small.
-        llist_t *small = (llist_t *)
-                         ((intptr_t) a * (a->head->data <= b->head->data) +
-                          (intptr_t) b * (a->head->data > b->head->data));
-        //Extract the first node of the chosen list and put to the new list
-        if (current) {
-            current->next = small->head;
-            current = current->next;
-        } else {
-            _list->head = small->head;
-            current = _list->head;
-        }
-        small->head = small->head->next;
-        --small->size;
-        ++_list->size;
-        current->next = NULL;
-    }
-    //Append the remaining nodes
-    llist_t *remaining = (llist_t *) ((intptr_t) a * (a->size > 0) +
-                                      (intptr_t) b * (b->size > 0));
-    if (current) current->next = remaining->head;
-    _list->size += remaining->size;
-    free(a);
-    free(b);
-    return _list;
-}
-
-llist_t *merge_sort(llist_t *list)
-{
-    if (list->size < 2)
-        return list;
-    int mid = list->size / 2;
-    llist_t *left = list;
-    llist_t *right = list_new();
-    right->head = list_get(list, mid);
-    right->size = list->size - mid;
-    list_get(list, mid - 1)->next = NULL;
-    left->size = mid;
-    return merge_list(merge_sort(left), merge_sort(right));
-}
-
-void merge(void *data)
+void merge_thread_lists(void *data)
 {
     llist_t *_list = (llist_t *) data;
     if (_list->size < (uint32_t) data_count) {
@@ -80,8 +33,8 @@ void merge(void *data)
             tmp_list = NULL;
             pthread_mutex_unlock(&(data_context.mutex));
             task_t *_task = (task_t *) malloc(sizeof(task_t));
-            _task->func = merge;
-            _task->arg = merge_list(_list, _t);
+            _task->func = merge_thread_lists;
+            _task->arg = sort_n_merge(_list, _t);
             tqueue_push(pool->queue, _task);
         }
     } else {
@@ -124,7 +77,7 @@ void cut_func(void *data)
         tqueue_push(pool->queue, _task);
     } else {
         pthread_mutex_unlock(&(data_context.mutex));
-        merge(merge_sort(list));
+        merge_thread_lists(merge_sort(list));
     }
 }
 
